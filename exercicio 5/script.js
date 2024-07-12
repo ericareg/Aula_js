0f4fb3ff8a1597b1d936f89cd28626cb87d722b3
erica regnier <ericaregnier@tecgraf.puc-rio.br>
2024-07-12 09:16:45 -0300

    adicionando exercicios 1 e 5 novamente

---
+++ exercicio 5/script.js
@@ -0,0 +1,13 @@
+ // Pede dois números inteiros ao usuário
+ const numero1 = parseInt(prompt("Digite um número inteiro:"));
+ const numero2 = parseInt(prompt("Digite um número inteiro:"));
+
+
+ // Calcula a soma dos números
+ const soma = numero1 + numero2;
+
+ // Exibe o resultado
+ console.log(`A soma dos numeros ${numero1} e ${numero2} é ${soma}.`);
+
+ // resultado como alert também
+ alert(`${numero1} + ${numero2} = ${soma}.`);
