787433ec045c5317a0653ba6829e008203ea2c36
erica regnier <ericaregnier@tecgraf.puc-rio.br>
2024-07-12 09:27:49 -0300

    exercicio 7

---
+++ exercicio 7/script.js
@@ -0,0 +1,13 @@
+
+  const nota1 = parseFloat(prompt("Digite a primeira nota:"));
+  const nota2 = parseFloat(prompt("Digite a segunda nota:"));
+  const nota3 = parseFloat(prompt("Digite a terceira nota:"));
+ 
+ 
+  const soma = nota1 + nota2 + nota3;
+  const media = soma/3;
+ 
+  console.log(`A media das notas é ${media}.`);
+ 
+
+  alert(`A media das notas é ${media}.`);
