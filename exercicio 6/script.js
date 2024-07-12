779b99e2bf752b980dea2e6dffcc0cc419b911a3
erica regnier <ericaregnier@tecgraf.puc-rio.br>
2024-07-12 09:21:28 -0300

    exercicio 6

---
+++ exercicio 6/script.js
@@ -0,0 +1,12 @@
+ // Pede um número inteiro ao usuário
+ const numero = parseInt(prompt("Digite um número inteiro:"));
+
+
+ // Calcula o quadrado do número
+ const quadrado = numero * numero;
+
+ // Exibe o resultado
+ console.log(`O quadrado do numero ${numero} é ${quadrado}.`);
+
+ // resultado como alert também
+ alert(`${numero} ao quadrado é ${quadrado}.`);
