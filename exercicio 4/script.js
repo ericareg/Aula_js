3a61bbf33b20163cdbb29b577c8ad3725dc1285f
erica regnier <ericaregnier@tecgraf.puc-rio.br>
2024-07-12 09:06:42 -0300

    adicionei o exercicio 4 de js

---
+++ exercicio 4/script.js
@@ -0,0 +1,11 @@
+ // Pede um número inteiro ao usuário
+ const numero = parseInt(prompt("Digite um número inteiro:"));
+
+ // Calcula o dobro do número
+ const dobro = numero * 2;
+
+ // Exibe o resultado
+ console.log(`O dobro de ${numero} é ${dobro}.`);
+
+ // resultado como alert também
+ alert(`O dobro de ${numero} é ${dobro}.`);
