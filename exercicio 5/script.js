89c81210e37c2db6f052504e5c3527a5b8194e3f
erica regnier <ericaregnier@tecgraf.puc-rio.br>
2024-07-12 09:13:35 -0300

    adicionei o exercicio 5

---
+++ exercicio 5/script.js
@@ -0,0 +1,13 @@
+ // Pede um número inteiro ao usuário
+ const numero1 = parseInt(prompt("Digite um número inteiro:"));
+ const numero2 = parseInt(prompt("Digite um número inteiro:"));
+
+
+ // Calcula o dobro do número
+ const soma = numero1 + numero2;
+
+ // Exibe o resultado
+ console.log(`A soma dos numeros ${numero1} e ${numero2} é ${soma}.`);
+
+ // resultado como alert também
+ alert(`${numero1} + ${numero2} = ${soma}.`);
